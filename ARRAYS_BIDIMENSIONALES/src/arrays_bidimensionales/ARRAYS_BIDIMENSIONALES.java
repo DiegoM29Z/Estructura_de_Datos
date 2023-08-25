a04e9a18bac32dab2cda1cd4008cbefa0586f350
Diego <diego290904m@gmail.com>
2023-08-25 12:10:45 -0500

    arreglos bidimensionales

---
+++ ARRAYS_BIDIMENSIONALES/src/arrays_bidimensionales/ARRAYS_BIDIMENSIONALES.java
@@ -0,0 +1,52 @@
+
+package arrays_bidimensionales;
+
+import javax.swing.JOptionPane;
+
+
+public class ARRAYS_BIDIMENSIONALES {
+
+    
+    public static void main(String[] args) {
+        int notas [] [] =new int [2] [3];
+        
+         
+        for (int ren= 0; ren<notas.length ; ren++)
+        {
+            for (int col = 0; col < notas[ren].length; col++)
+                
+            {
+                
+                
+     notas[ren][col]=Integer.parseInt(JOptionPane.showInputDialog("ingresa la nota del estudiante "+(col+1)));
+                  
+                 
+            
+            }
+            
+        }
+        
+        for (int ren []:notas)
+            
+        {                System.out.println(" ");
+
+            for (int nota:ren)
+                
+            {
+                System.out.println(nota+"");
+            }
+            
+        }
+    /*  for (int ren= 0; ren<notas.length ; ren++)
+        {
+            System.out.println("");
+            for (int col = 0; col < notas[ren].length; col++)
+                
+            {
+                System.out.print(notas[ren][col]+" ");
+        }
+        
+    }*/
+    
+    }
+}
---
+++ MATRICES_/src/matrices/Matrices.java
@@ -0,0 +1,12 @@
+
+package matrices;
+
+
+public class Matrices {
+
+    
+    public static void main(String[] args) {
+
+    }
+    
+}
